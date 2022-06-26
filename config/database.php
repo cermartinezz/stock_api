261c8d7acaccb2a9797f37580ca5df42d5598aeb
cermartinezz <cesarmartinez9311@gmail.com>
2022-06-26 12:18:52 -0600

    set db settings

--- config/database.php
+++ config/database.php
@@ -4,8 +4,8 @@ return [
     'db' => [
         'driver'    => 'mysql',
         'host'      => 'localhost',
-        'port'      => $_ENV["DB_PORT"] ?? '3306',
-        'database'  => $_ENV["DB_DATABASE"] ?? 'php_jobsitykk',
+        'port'      => $_ENV["DB_PORT"] ?? '5306',
+        'database'  => $_ENV["DB_DATABASE"] ?? 'php_jobsity',
         'username'  => $_ENV["DB_USER"] ?? 'root',
         'password'  => $_ENV["DB_PASSWORD"] ?? '',
         'charset'   => 'utf8',
